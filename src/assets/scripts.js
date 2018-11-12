76c9cb6317deed51c1966261f037000f4502b4e6
Amine M. Boulouma <amine@boulouma.com>
2018-11-12 10:11:19 +0100

    Integrated bootstrap to home search page

--- src/assets/scripts.js
+++ src/assets/scripts.js
@@ -1,36 +1,3 @@
-dataTest = [
-        {
-            "imageURL":"http://www.konbini.com/wp-content/blogs.dir/11/files/2017/08/onepiece-480x279.jpg", 
-            "title":"One Piece", 
-            "author":"", 
-            "demographic":"", 
-            "genre":"", 
-            "firstPublicationDate":"", 
-            "lastPublicationDate":"",
-            "volumes":""
-        },
-        {
-            "imageURL":"https://medias.comixtrip.fr/wp-content/uploads/2014/10/naruto-shippuden-02-1200x675.jpg", 
-            "title":"Naruto", 
-            "author":"", 
-            "demographic":"", 
-            "genre":"", 
-            "firstPublicationDate":"", 
-            "lastPublicationDate":"",
-            "volumes":""
-        },
-        {
-            "imageURL":"https://myanimelist.cdn-dena.com/images/anime/3/40451.jpg", 
-            "title":"Bleach", 
-            "author":"", 
-            "demographic":"", 
-            "genre":"", 
-            "firstPublicationDate":"", 
-            "lastPublicationDate":"",
-            "volumes":""
-        }
-    ]
-
 function getResults(mangasData){
     for(var i=0; i < mangasData.length; i++){
         appendResult(i, mangasData[i]);
@@ -40,8 +7,9 @@ function getResults(mangasData){
 
 function appendResult(id, mangaData){
     var manga = $("<div>").load("assets/templates/manga-item.html", function() {
+        manga.attr("class", "manga-item")
         manga.attr("id", "manga-item-" + id);
-        manga.find(".image-url").attr("src", mangaData["imageURL"]);
+        manga.find(".image").attr("src", mangaData["imageURL"]);
         manga.find(".title").text(mangaData["title"]);
         manga.find(".author").text(mangaData["author"]);
         manga.find(".demografic").text(mangaData["demografic"]);
@@ -53,12 +21,41 @@ function appendResult(id, mangaData){
     });
 }
 
-function test(){
-    for (var i=1; i<4; i++){
-        appendResult(0,i);
-    }
-}
 
 function empty(){
     $("#results").empty();
 }
+
+
+dataTest = [
+    {
+        "imageURL":"http://www.konbini.com/wp-content/blogs.dir/11/files/2017/08/onepiece-480x279.jpg", 
+        "title":"One Piece", 
+        "author":"", 
+        "demographic":"", 
+        "genre":"", 
+        "firstPublicationDate":"", 
+        "lastPublicationDate":"",
+        "volumes":""
+    },
+    {
+        "imageURL":"https://medias.comixtrip.fr/wp-content/uploads/2014/10/naruto-shippuden-02-1200x675.jpg", 
+        "title":"Naruto", 
+        "author":"", 
+        "demographic":"", 
+        "genre":"", 
+        "firstPublicationDate":"", 
+        "lastPublicationDate":"",
+        "volumes":""
+    },
+    {
+        "imageURL":"https://static.hitek.fr/img/actualite/2016/08/18/fb_bleach-012.jpg", 
+        "title":"Bleach", 
+        "author":"", 
+        "demographic":"", 
+        "genre":"", 
+        "firstPublicationDate":"", 
+        "lastPublicationDate":"",
+        "volumes":""
+    }
+]
