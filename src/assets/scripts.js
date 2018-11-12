83244534d9ca7748def8e2bc4564cde8c79b0479
Amine M. Boulouma <amine@boulouma.com>
2018-11-12 08:46:51 +0100

    Updated dataTest

--- src/assets/scripts.js
+++ src/assets/scripts.js
@@ -1,8 +1,7 @@
-dataTest = {
-    "mangas": [
+dataTest = [
         {
             "imageURL":"", 
-            "title":"", 
+            "title":"Manga1", 
             "author":"", 
             "demographic":"", 
             "genre":"", 
@@ -12,7 +11,7 @@ dataTest = {
         },
         {
             "imageURL":"", 
-            "title":"", 
+            "title":"Manga2", 
             "author":"", 
             "demographic":"", 
             "genre":"", 
@@ -22,21 +21,27 @@ dataTest = {
         },
         {
             "imageURL":"", 
-            "title":"", 
+            "title":"Manga3", 
             "author":"", 
             "demographic":"", 
             "genre":"", 
             "firstPublicationDate":"", 
             "lirstPublicationDate":"",
             "volumes":""
-        },
+        }
     ]
+
+function getResults(mangasData){
+    for(var i=0; i < mangasData.length; i++){
+        appendResult(mangasData[i], i);
+    }
 }
 
 function appendResult(mangaData, id){
     var manga = $("<div>").load("assets/templates/manga-item.html");
     manga.attr("id", "manga-item-" + id);
     $("#results").append(manga);
+    $("#manga-item-" + id + " .title").text(mangaData["title"]);
 }
 
 function test(){
