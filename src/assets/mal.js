d8fe64d26a2b79a5d3e843697980b12a4e3212e5
Paul Du <du.paul136@gmail.com>
2018-11-13 20:11:24 +0100

    Added search with MAL

--- src/assets/mal.js
+++ src/assets/mal.js
@@ -1,30 +1,48 @@
-function searchMALByName(name, onSuccess)
-{
-    $.get("https://api.jikan.moe/v3/search/manga",
-          {"q":name, "type":"manga", "page":1},
-          function(results) {
-        var mangasData = [];
-        var promises = [];
-        for (var i in results["results"])
-        {
-            var summary = results["results"][i];
-            var request = $.get("https://api.jikan.moe/v3/manga/"+summary["mal_id"],function(manga) {
+var MAL = {
+    pg13:3,
+    
+    findManga : function(id)
+    {
+        return new Promise((resolve, reject) => {
+            var request = $.get("https://api.jikan.moe/v3/manga/"+id, manga => {
+                var authors = manga["authors"].map(a => a["name"]).join(", ");
+                var genres = manga["genres"].map(g => g["name"]).join(", ");
+                
                 var data = {
                     "imageURL": manga["image_url"],
                     "title": manga["title"],
-                    "firstPublicationDate": manga["start_date"],
-                    "lastPublicationDate": manga["end_date"],
+                    "description": manga["synopsis"],
+                    "author": authors,
+                    "genres": genres,
+                    "firstPublicationDate": manga["published"]["from"],
+                    "lastPublicationDate": manga["published"]["to"],
                     "volumes": manga["volumes"]
                 };
                 
-                mangasData[this.i] = data;
-            }.bind({i:i}));
-            
-            promises.push(request);
-        }
-        
-        $.when.apply(null, promises).done(function() {
-            getResults(mangasData);
+                resolve(data);
+            }).fail(data => reject(data));
         });
-    });
+    },
+    
+    searchByName : function (name, onSuccess)
+    {
+        $.get("https://api.jikan.moe/v3/search/manga",
+            {"q":name, "type":"manga", "rated":this.pg13, "limit":10, "page":1},
+            function(results) {
+                var promises = [];
+                for (var i in results["results"])
+                {
+                    var summary = results["results"][i];
+                    promises[i] = MAL.findManga(summary["mal_id"]);
+                }
+                
+                function nextPromise(i) {
+                    promises[i].then(manga => onSuccess(manga));
+                    if (i+1 < promises.length)
+                        promises[i].then(manga => nextPromise(i+1,manga));
+                }
+                
+                nextPromise(0);
+        });
+    }
 }
--- src/assets/scripts.js
+++ src/assets/scripts.js
@@ -1,29 +1,42 @@
-function getResults(mangasData){
-    for(var i in mangasData){
-        appendResult(i, mangasData[i]);
+var SearchBar = {
+    search : function(form) {
+        MangaList.empty();
+        MAL.searchByName($(form).find("[name=manga-title]").val(), MangaList.append);
     }
 }
 
+var MangaList = {
+    mangaItemId: 0,
+    
+    appendList : function(mangasData) {
+        for(var i in mangasData) {
+            this.append(mangasData[i]);
+        }
+    },
 
-function appendResult(id, mangaData){
-    var manga = $("<div>").load("assets/templates/manga-item.html", function() {
-        manga.attr("class", "manga-item card col-md-3 ");
-        manga.attr("id", "manga-item-" + id);
-        manga.find(".image").attr("src", mangaData["imageURL"]);
-        manga.find(".title").text(mangaData["title"]);
-        manga.find(".description").text(mangaData["description"]);
-        manga.find(".author").text(mangaData["author"]);
-        manga.find(".demografic").text(mangaData["demografic"]);
-        manga.find(".type").text(mangaData["type"]);
-        manga.find(".first-publication-date").text(mangaData["firstPublicationDate"]);
-        manga.find(".last-publication-date").text(mangaData["lastPublicationDate"]);
-        manga.find(".volumes").text(mangaData["volumes"]);
-        $("#results").append(manga);
-        $("#results").append("<br>");
-    });
-}
+    append : function(mangaData) {
+        var manga = $("<div>").load("assets/templates/manga-item.html", function() {
+            id = this.mangaItemId;
+            this.mangaItemId++;
+            
+            manga.attr("class", "manga-item card col-md-4");
+            manga.attr("id", "manga-item-" + id);
+            manga.find(".image").attr("src", mangaData["imageURL"]);
+            manga.find(".title").text(mangaData["title"]);
+            manga.find(".description").text(mangaData["description"]);
+            manga.find(".author").text(mangaData["author"]);
+            manga.find(".demographic").text(mangaData["demographic"]);
+            manga.find(".genres").text(mangaData["genres"]);
+            manga.find(".first-publication-date").text(mangaData["firstPublicationDate"]);
+            manga.find(".last-publication-date").text(mangaData["lastPublicationDate"]);
+            manga.find(".volumes").text(mangaData["volumes"]);
+            $("#results").append(manga);
+            $("#results").append("<br>");
+        });
+    },
 
-
-function empty(){
-    $("#results").empty();
+    empty : function() {
+        this.mangaItemId = 0;
+        $("#results").empty();
+    }
 }
